b6fd6218d98b6ed5f2b81ad805a0410513503244
TimeTravelRules <112023003+TimeTravelRules@users.noreply.github.com>
2026-06-06 05:41:53 +0000

    realizei o segundo desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    //Criei as variavéis simples da carta 1
-    char estado1;
-    char codigo1[4];
-    char cidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+    //Criei as variavéis simples, agrupando as iguais na mesma linha
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char cidade1[50], cidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
 
-    //Copiei os mesmos dados para a carta 2
-    char estado2;
-    char codigo2[4];
-    char cidade2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+    float densidade_populacao, pib_capta;
+    densidade_populacao = (float) populacao1 / area1;
+    pib_capta = pib1 / populacao1;
     
 
     printf("Cadastro da Carta 1\n");
@@ -75,6 +70,11 @@ int main() {
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("Densidade populacional: %.2f\n", densidade_populacao);
+    printf("Pib per capta: %.2f\n", pib_capta);
+
+    densidade_populacao = (float) populacao2 / area2;
+    pib_capta = pib2 / populacao2;
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -84,6 +84,8 @@ int main() {
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("Densidade populacional: %.2f\n", densidade_populacao);
+    printf("Pib per capta: %.2f\n", pib_capta);
 
     //encerrei a função main, finalizando o codigo
     return 0;
