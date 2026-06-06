acccb3a2971fcf4ae962f69e27fea8e57a1c64a1
TimeTravelRules <112023003+TimeTravelRules@users.noreply.github.com>
2026-06-06 05:31:11 +0000

    Tentando simplificar o código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    //Criei as variavéis simples, agrupando as iguais na mesma linha
-    char estado1, estado2;
-    char codigo1[4], codigo2[4];
-    char cidade1[50], cidade2[50];
-    int populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2
-    int pontosTuristicos1, pontosTuristicos2;
+    //Criei as variavéis simples da carta 1
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
 
-    float densidade_populacao, pib_capta;
-    
+    //Copiei os mesmos dados para a carta 2
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
     
 
     printf("Cadastro da Carta 1\n");
